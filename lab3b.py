59d75fa49dce1e00b266c34fdaa591d983980ac3
mitch <mgregoris2@myseneca.ca>
2024-10-12 11:12:24 -0400

    Module 3b completed

---
+++ lab3b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Author: Mitch Gregoris
+# Author ID: 133349191
+# Date Created: 2023-10-12
+
+def sum_numbers(number1, number2):
+    # Make this function add number1 and number2 and return the value
+    sum = number1 + number2
+    return sum
+
+def subtract_numbers(number1, number2):
+    # Make this function subtract number1 and number2 and return the value
+    # Remember to make sure the function accepts 2 arguments
+    sum = number1 - number2
+    return sum
+
+def multiply_numbers(number1, number2):
+    # Make this function multiply number1 and number2 and return the value
+    # Remember to make sure the function accepts 2 arguments
+    sum = number1 * number2
+    return sum
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
